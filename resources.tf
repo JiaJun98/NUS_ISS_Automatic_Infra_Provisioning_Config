78ff6b4621ea81aeddda6489dcfe0c8c0b95c50a
JiaJun <gohjiajun.98@gmail.com>
2024-06-24 08:41:44 +0000

    Adding README and resources.tf

--- resources.tf
+++ resources.tf
@@ -4,3 +4,32 @@ resource "docker_image" "bgg-database" {
 resource "docker_image" "bgg-backend" {
     name = "chukmunnlee/bgg-backend: ${var.backend_version}"
 }
+
+resource "docker_network" "bgg-net" {
+  name = "my-sgp-bgg-net"
+}
+
+resource "docker_volume" "data-vol" {
+  name = "my-sgp-data-vol"
+}
+
+resource "docker_container" "bgg-database" {
+  networks_advanced {
+    name = docker_network.bgg-net.id
+  }
+
+  ports {
+    internal = 3306
+    external = 3306
+  }
+
+  volumes {
+    volume_name = docker_volume.data-vol.name
+    container_path = "/var/lib/mysql"
+  }
+
+}
+
+
+resource "docker_container" "bgg-backend" {
+}
