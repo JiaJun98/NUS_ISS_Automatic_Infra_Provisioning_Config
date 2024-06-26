ce00f291afee082ddaf3a3cb5a1bdeceac1e01d7
Goh Jia Jun <59722769+JiaJun98@users.noreply.github.com>
2024-06-26 09:25:01 +0800

    Sync changes with instructors

--- aipc-workshop-day2/resources.tf
+++ aipc-workshop-day2/resources.tf
@@ -7,18 +7,27 @@ resource "digitalocean_droplet" "codeserver" {
   image = var.do_image
   region = var.do_region
   size = var.do_size
+  
+  ssh_keys = [ data.digitalocean_ssh_key.aipc.id ] #Dont use fingerprint normally it wont resolve the id one
+}
 
-  ssh_keys = [data.digitalocean_ssh_key.aipc.id] #Dont use fingerprint normally it wont resolve the id one
+resource "local_file" "root_at_codeserver" {
+    filename = "root@${digitalocean_droplet.codeserver.ipv4_address}"
+    content = ""
+    file_permission = "0444"
 }
 
 resource "local_file" "inventory" {
-  content = templatefile("inventory.yaml.tftpl", {
+    content = templatefile("inventory.yaml.tftpl", {
         codeserver_ip = digitalocean_droplet.codeserver.ipv4_address
         ssh_private_key = var.ssh_private_key
         codeserver_password = var.codeserver_password
-        codeserver_domain = "code-server-${digitalocean_droplet.codeserver.ipv4_address}.example.com" #Any domain will do
-  })
+        codeserver_domain = "code-server-${digitalocean_droplet.codeserver.ipv4_address}.nip.io"
+    })
+    filename = "inventory.yaml"
+    file_permission = "0444"
+}
 
-  filename = "inventory.yml"
-  file_permission = "0444" 
+output codeserver_ip {
+    value = digitalocean_droplet.codeserver.ipv4_address
 }
